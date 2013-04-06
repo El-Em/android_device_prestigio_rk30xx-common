b440bc564a39ff769a6221a2aae03ecfb2b8f6ac
Adam Farden <adam@farden.cz>
2013-04-07 00:11:28 +0200

    Add full framework overlay

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,7 @@ USE_OPENGL_RENDERER := true
 TARGET_USES_ION := true
 BOARD_EGL_CFG := device/prestigio/rk30xx-common/config/egl.cfg
 
-#Bluetooth
+# Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 
 # Webkit
